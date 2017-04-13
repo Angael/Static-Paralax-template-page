ba9ce76b65241b6b484e3d3514db57afdfa078e0
Angael <Krzysztofwidacki@gmail.com>
2017-04-13 11:00:14 +0200

    init version

---
+++ index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Blog page design</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="stylesheet" media='screen and (min-width: 100px)' href="style.css" />
+	<link href="https://fonts.googleapis.com/css?family=Raleway:100,200" rel="stylesheet">
+    <link rel="stylesheet" media='screen and (max-width: 100px)' href="stylemobile.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<script src="jquery-3.1.1.js"></script>
+	<script src="js/mobileGUI.js"></script>
+  </head>
+  <body>
+    
+	<div class="stackElement whiteText" >
+		<div class="bgParallax" style="background-image: url('https://previews.123rf.com/images/stockbroker/stockbroker1507/stockbroker150703732/42252883-Group-Of-Business-People-Listening-To-Colleague-Addressing-Office-Meeting-Stock-Photo.jpg');">
+		</div>
+		<div class="containerTitle">
+			<h1>Twoja Firma</h1>
+			<br>
+			<p>Ponad 20 klientów zadowolonych z moich usług
+			</p>
+		</div>
+	</div>
+	
+	<div class="stackElement bgGrey1">
+		<div class="containerTitle">
+			<div class="column-parent">
+				<div class="column-element">
+					<img src="icons/dollar.png" class="icon"/>
+					<h3>Tanio</h3><hr>
+					Nasza firma zapewnia najtańsze usługi itp itd
+				</div>
+				<div class="column-element">
+					<img src="icons/fast.png" class="icon"/>
+					<h3>Szybko</h3><hr>
+					Wszystko co robimy mieści się w ramach czasowych które wyznaczymy. Nigdy nie opóźniamy.
+					
+				</div>
+				<div class="column-element">
+					<img src="icons/like.png" class="icon"/>
+					<h3>Dobrze</h3><hr>
+					Dzięki mojemu dużemu doświadczeniu z daną usługą wykonuje ją solidnie.
+				</div>
+				<div class="column-element">
+					<img src="icons/time.png" class="icon"/>
+					<h3>Punktualność</h3><hr>
+					Nigdy nie spóźniam się.
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="stackElement">
+		<div class="bgParallax" style="background-image: url('http://s.hswstatic.com/gif/future-9.jpg');">
+		</div>
+		<div class="containerTitle whiteText">
+			
+			<h2>Kim jestem?</h2>
+		</div>
+	</div>
+	<div class="stackElement">
+		<div class="containerContent">
+			<div class="column-parent">
+				<div class="column-element two">
+					
+					<h3>O mnie</h3><hr>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec ex non urna posuere tempus ut eu nunc. Donec at ligula mauris. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec mollis pulvinar convallis. Etiam rhoncus pulvinar felis, sed convallis nisl. Nam laoreet porttitor augue, vitae interdum sem tincidunt id. Phasellus ornare pulvinar elit, nec porta ante ultricies ut. Nullam at tristique purus. In eget ultricies arcu. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Curabitur a quam sed ante mollis imperdiet id et velit. Nulla feugiat laoreet lectus, rhoncus mattis neque congue quis. Donec eget purus vel felis fringilla feugiat at eu metus. Aliquam lacinia libero id odio rutrum feugiat.
+
+				</div>
+				<div class="column-element two">
+					<img src="http://l7.alamy.com/zooms/1659aa7d35334fcc8c272d69be1f309b/private-secretary-to-britains-prince-charles-speaks-to-media-following-h0yx69.jpg" class="mainImage">
+				</div>
+			</div>
+			
+		</div>
+	</div>
+	
+	<div class="stackElement">
+		<div class="containerTitle whiteText">
+			<h2>Co oferuje?</h2>
+		</div>
+	</div>
+	
+	<div class="stackElement">
+		<div class="bgParallax" style="background-image: url('https://static.pexels.com/photos/10651/photo-1432821596592-e2c18b78144f.jpeg');">
+		</div>
+		<div class="containerTitle whiteText">
+			<h2>Cennik</h2>
+		</div>
+	</div>
+	
+	<div class="stackElement">
+		<div class="bgParallax" style="background-image: url('https://static.pexels.com/photos/10651/photo-1432821596592-e2c18b78144f.jpeg');">
+		</div>
+		<div class="containerTitle whiteText">
+			<h2>Klienci</h2>
+		</div>
+	</div>
+	
+	<div class="stackElement">
+		<div class="bgParallax" style="background-image: url('https://static.pexels.com/photos/10651/photo-1432821596592-e2c18b78144f.jpeg');">
+		</div>
+		<div class="containerTitle whiteText">
+			<h2>Kontakt</h2>
+		</div>
+	</div>
+	
+	<div class="stackElement">
+		<div class="bgParallax" style="background-image: url('https://static.pexels.com/photos/10651/photo-1432821596592-e2c18b78144f.jpeg');">
+		</div>
+		<div class="containerTitle whiteText">
+			<h2>Położenie</h2>
+		</div>
+	</div>
+	
+    <footer>
+	© Copyright - "twoja firma" • Designed by <u>Krzysztof Widacki</u><br>
+	Icon Pack License : <u>"PDF"</u>
+	</footer>
+  <body>
+<html>
